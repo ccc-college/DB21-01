5bc62ecb97934c0a7599da5a084f9beb855c292c
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-05-24 17:26:18 +0900

    Add files via upload

---
+++ DB21-02.sql
@@ -0,0 +1,19 @@
+/* CREATE DATABASE tdb00 DEFAULT CHARACTER SET utf8; */
+/* use tdb00; */
+use study;
+
+DROP TABLE IF EXISTS slist;
+CREATE TABLE slist (
+id int not null primary key,
+name varchar(32),
+price int,
+num int
+);
+INSERT INTO slist (id, name, price, num) VALUES (1,'鶏肉',350,2);
+INSERT INTO slist (id, name, price, num) VALUES (2,'大根',100,1);
+INSERT INTO slist (id, name, price, num) VALUES (3,'卵',200,3);
+INSERT INTO slist (id, name, price, num) VALUES (4,'納豆',150,3);
+INSERT INTO slist (id, name, price, num) VALUES (5,'もやし',30,5);
+INSERT INTO slist (id, name, price, num) VALUES (5,'牛肉',800,3);
+INSERT INTO slist (id, name, price, num) VALUES (5,'キャベツ',250,5);
+INSERT INTO slist (id, name, price, num) VALUES (5,'じゃがいも',110,10);
